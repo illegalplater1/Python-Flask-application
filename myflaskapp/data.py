6a909d7e690c59a6d2b228b021afcaec278ab16c
illegalplayer1 <indulgelx@gmail.com>
2018-03-29 01:00:52 +0800

    First commit

---
+++ myflaskapp/data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article one',
+            'body':'sdfsdf',
+            'sdfsdf':'sdfsdf',
+            'sdf':'sdfsdf'
+
+        },
+        {
+            'id':2,
+            'title':'Article one',
+            'body':'sdfsdf',
+            'sdfsdf':'sdfsdf',
+            'sdf':'sdfsdf'
+
+        },
+        {
+            'id':3,
+            'title':'Article one',
+            'body':'sdfsdf',
+            'sdfsdf':'sdfsdf',
+            'sdf':'sdfsdf'
+
+        }
+    ]
+    return articles
